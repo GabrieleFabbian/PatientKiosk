a05b5905bbb6bcfc69205cff0fd573cf7ecce8fc
GabrieleFabbian <gabripiano2001@gmail.com>
2026-08-07 20:33:35 +0200

    aggiunta la gestione degli errori nel caricamento dei json questionari

--- app/src/main/java/com/gabriele/patientkiosk/repository/QuestionnaireRepository.kt
+++ app/src/main/java/com/gabriele/patientkiosk/repository/QuestionnaireRepository.kt
@@ -1,6 +1,7 @@
 package com.gabriele.patientkiosk.repository
 
 import android.content.Context
+import android.util.Log
 import com.gabriele.patientkiosk.model.Answer
 import com.gabriele.patientkiosk.model.Interpretation
 import com.gabriele.patientkiosk.model.Question
@@ -9,63 +10,65 @@ import org.json.JSONObject
 
 class QuestionnaireRepository(private val context: Context) {
 
-    fun loadQuestionnaire(fileName: String): Questionnaire {
-        val json = context.assets.open(fileName).bufferedReader().use { it.readText() }
-        val obj = JSONObject(json)
+    fun loadQuestionnaire(fileName: String): Questionnaire? {
+        return try {
+            val json = context.assets.open(fileName).bufferedReader().use { it.readText() }
+            val obj = JSONObject(json)
 
-        val questionsArray = obj.getJSONArray("questions")
-        val questions = mutableListOf<Question>()
+            val questionsArray = obj.getJSONArray("questions")
+            val questions = mutableListOf<Question>()
 
-        for (i in 0 until questionsArray.length()) {
-            val q = questionsArray.getJSONObject(i)
-            val answersArray = q.getJSONArray("answers")
-            val answers = mutableListOf<Answer>()
+            for (i in 0 until questionsArray.length()) {
+                val q = questionsArray.getJSONObject(i)
+                val answersArray = q.getJSONArray("answers")
+                val answers = mutableListOf<Answer>()
 
-            for (j in 0 until answersArray.length()) {
-                val a = answersArray.getJSONObject(j)
-                answers.add(Answer(a.getString("text"), a.getInt("score")))
+                for (j in 0 until answersArray.length()) {
+                    val a = answersArray.getJSONObject(j)
+                    answers.add(Answer(a.getString("text"), a.getInt("score")))
+                }
+
+                questions.add(
+                    Question(
+                        id = q.getInt("id"),
+                        text = q.getString("text"),
+                        answers = answers,
+                        type = q.optString("type", "")
+                    )
+                )
             }
 
-            questions.add(
-                Question(
-                    id = q.getInt("id"),
-                    text = q.getString("text"),
-                    answers = answers,
-                    type = q.optString("type", "")
+            val interpretationsArray = obj.getJSONArray("interpretations")
+            val interpretations = mutableListOf<Interpretation>()
+
+            for (i in 0 until interpretationsArray.length()) {
+                val interp = interpretationsArray.getJSONObject(i)
+                interpretations.add(
+                    Interpretation(
+                        interp.getInt("min"),
+                        interp.getInt("max"),
+                        interp.getString("label")
+                    )
                 )
+            }
+
+            Questionnaire(
+                id = obj.getString("id"),
+                name = obj.getString("name"),
+                description = obj.getString("description"),
+                category = obj.getString("category"),
+                questions = questions,
+                maxScore = obj.optInt("maxScore", obj.optInt("maxRawScore", obj.optInt("maxScoreAnxiety", 0) + obj.optInt("maxScoreDepression", 0))),
+                interpretations = interpretations
             )
+        } catch (e: Exception) {
+            Log.e("QuestionnaireRepository", "Errore nel caricamento di $fileName: ${e.message}")
+            null
         }
-
-        val interpretationsArray = obj.getJSONArray("interpretations")
-        val interpretations = mutableListOf<Interpretation>()
-
-        for (i in 0 until interpretationsArray.length()) {
-            val interp = interpretationsArray.getJSONObject(i)
-            interpretations.add(
-                Interpretation(
-                    interp.getInt("min"),
-                    interp.getInt("max"),
-                    interp.getString("label")
-                )
-            )
-        }
-
-        return Questionnaire(
-            id = obj.getString("id"),
-            name = obj.getString("name"),
-            description = obj.getString("description"),
-            category = obj.getString("category"),
-            questions = questions,
-            maxScore = obj.optInt("maxScore", obj.optInt("maxRawScore", obj.optInt("maxScoreAnxiety", 0) + obj.optInt("maxScoreDepression", 0))),
-            interpretations = interpretations
-        )
     }
 
     fun loadAll(): List<Questionnaire> {
-        return listOf(
-            loadQuestionnaire("dlqi.json"),
-            loadQuestionnaire("hads.json"),
-            loadQuestionnaire("who5.json")
-        )
+        return listOf("dlqi.json", "hads.json", "who5.json")
+            .mapNotNull { loadQuestionnaire(it) }
     }
 }
